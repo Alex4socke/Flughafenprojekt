8837c427d36b9ced5a0e0135266966c766adfb84
Uhlmann, Jonathan <jonathan.uhlmann@wolterskluwer.com>
2021-04-23 11:07:07 +0200

    task 2 almost done

--- src/controll/Main.java
+++ src/controll/Main.java
@@ -2,8 +2,10 @@ package controll;
 
 import test.Client;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Client.testFlugzeugClass();
     }
 }
--- src/test/Client.java
+++ src/test/Client.java
@@ -2,9 +2,24 @@ package test;
 
 import classes.Flugzeug;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Client {
-    public static void testFlugzeugClass() {
-        Flugzeug flugzeug = new Flugzeug(0, "02:03", 0);
+    public static void testFlugzeugClass() throws IOException {
+        int flugzeugtyp = 0;
+        String sollZeitLandung = "";
+        int fluggesellschaft = 0;
+        try {
+            flugzeugtyp = Integer.parseInt(getUserInput("flugzeugtyp"));
+            sollZeitLandung = getUserInput("sollZeitLandung");
+            fluggesellschaft = Integer.parseInt(getUserInput("fluggesellschaft"));
+        } catch (NumberFormatException e) {
+            System.out.println("Ob du dumm bist frage ich. Da steht ganz klar was du eingeben sollst...");
+            testFlugzeugClass();
+        }
+        Flugzeug flugzeug = new Flugzeug(flugzeugtyp, sollZeitLandung, fluggesellschaft);
         flugzeug.print();
         flugzeug.vergebeLandebahn(1);
         flugzeug.print();
@@ -16,5 +31,29 @@ public class Client {
         flugzeug.print();
         flugzeug.meldeGestartet("10:20");
         flugzeug.print();
+    }
+
+    private static String readInput() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader.readLine();
+    }
+
+    private static String getUserInput(String parameter) throws IOException {
+        String result = "";
+        switch (parameter){
+            case "flugzeugtyp":
+                System.out.println("Geben sie den Flugzeugtyp an (0 = Bowling, anderes gibts noch nicht):");
+                result = readInput();
+                break;
+            case "sollZeitLandung":
+                System.out.println("Geben sie die Sollzeit der Landung ein (hh:mm):");
+                result = readInput();
+                break;
+            case "fluggesellschaft":
+                System.out.println("Geben sie die Fluggesellschaft ein (0 = Microair, andere gibts noch nicht):");
+                result = readInput();
+                break;
+        }
+        return result;
     }
 }
